8c767c567a6d5867b0c630fb2466f79b5df0d900
Marcin P <marcin.pawlowski@hicron.com>
2025-01-08 16:06:28 +0100

    chore: add repository

---
+++ src/tests/e2e/csv.spec.ts
@@ -0,0 +1,67 @@
+import * as fs from "fs";
+import * as path from "path";
+import { expect, test } from "@playwright/test";
+import { parse } from "csv-parse/sync";
+
+const records = parse(fs.readFileSync(path.join("src/data/challenge.csv")), {
+  columns: true,
+  skip_empty_lines: true,
+  delimiter: ";",
+});
+
+let index: number = 1;
+test.use({ testIdAttribute: "ng-reflect-name" });
+
+test("RPG Challenge - input data from csv file", async ({ page }) => {
+  await page.goto("https://www.rpachallenge.com/");
+
+  await page.getByRole("button", { name: "Start" }).click();
+
+  for (const record of records) {
+    await expect(
+      page.getByRole("button", { name: `Round ${index}` })
+    ).toBeVisible();
+    await page.getByTestId("labelFirstName").fill(record.FirstName);
+    await page.getByTestId("labelLastName").fill(record.LastName);
+    await page.getByTestId("labelCompanyName").fill(record.CompanyName);
+    await page.getByTestId("labelRole").fill(record.RoleInCompany);
+    await page.getByTestId("labelAddress").fill(record.Address);
+    await page.getByTestId("labelEmail").fill(record.Email);
+    await page.getByTestId("labelPhone").fill(record.PhoneNumber);
+    await page.getByRole("button", { name: "Submit" }).click();
+    index++;
+  }
+
+  await expect(page.getByRole("button", { name: "Reset" })).toBeVisible();
+  await expect(page.locator(".message2")).toBeVisible();
+  const time = await page.locator(".message2").innerText();
+
+  //* Only last result
+  // fs.writeFile(
+  //   "src/output/data/rpg.txt",
+  //   `Congratulations! ${time} \n`,
+  //   "utf8",
+  //   (error) => {
+  //     if (error) {
+  //       console.error("An error occurred while writing to the file:", error);
+  //       return;
+  //     }
+  //     console.log("File written successfully");
+  //   }
+  // );
+
+  //* All results are saved
+  try {
+    fs.writeFileSync(
+      path.join("src/output/data/csv-rpg.txt"),
+      `Congratulations! ${time} \n`,
+      {
+        encoding: "utf8",
+        flag: "a+",
+      }
+    );
+    console.log("File written successfully");
+  } catch (err) {
+    console.error(err);
+  }
+});
---
+++ src/tests/e2e/xlsx.spec.ts
@@ -0,0 +1,62 @@
+import * as fs from "fs";
+import * as xlsx from "node-xlsx";
+import * as path from "path";
+import { expect, test } from "@playwright/test";
+
+const records = xlsx.parse(path.join("src/data/challenge.xlsx"));
+const record: string[][] = records[0].data;
+
+test.use({ testIdAttribute: "ng-reflect-name" });
+
+test("RPG Challenge - input data from xlsx file", async ({ page }) => {
+  await page.goto("https://www.rpachallenge.com/");
+
+  await page.getByRole("button", { name: "Start" }).click();
+
+  for (let index: number = 1; index < record.length; index++) {
+    await expect(
+      page.getByRole("button", { name: `Round ${index}` })
+    ).toBeVisible();
+    await page.getByTestId("labelFirstName").fill(record[index][0]);
+    await page.getByTestId("labelLastName").fill(record[index][1]);
+    await page.getByTestId("labelCompanyName").fill(record[index][2]);
+    await page.getByTestId("labelRole").fill(record[index][3]);
+    await page.getByTestId("labelAddress").fill(record[index][4]);
+    await page.getByTestId("labelEmail").fill(record[index][5]);
+    await page.getByTestId("labelPhone").fill(record[index][6].toString());
+    await page.getByRole("button", { name: "Submit" }).click();
+  }
+
+  await expect(page.getByRole("button", { name: "Reset" })).toBeVisible();
+  await expect(page.locator(".message2")).toBeVisible();
+  const time = await page.locator(".message2").innerText();
+
+  //* Only last result
+  // fs.writeFile(
+  //   "src/output/data/rpg.txt",
+  //   `Congratulations! ${time} \n`,
+  //   "utf8",
+  //   (error) => {
+  //     if (error) {
+  //       console.error("An error occurred while writing to the file:", error);
+  //       return;
+  //     }
+  //     console.log("File written successfully");
+  //   }
+  // );
+
+  //* All results are saved
+  try {
+    fs.writeFileSync(
+      path.join("src/output/data/xlsx-rpg.txt"),
+      `Congratulations! ${time} \n`,
+      {
+        encoding: "utf8",
+        flag: "a+",
+      }
+    );
+    console.log("File written successfully");
+  } catch (err) {
+    console.error(err);
+  }
+});
